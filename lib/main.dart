dce9793cdccca80c995698611a43d3d3915b8be5
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2023-03-31 14:55:00 +0700

    firebase core initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/modules/blocs/chatty_bloc_observer.dart';
+import 'package:social_app/modules/firebase/widgets/firebase_initializer.dart';
 import 'package:social_app/modules/pages/list_categories_page.dart';
 import 'package:social_app/modules/pages/list_posts_page.dart';
 
 void main() {
   Bloc.observer = ChattyBlocObserver();
-  runApp(const MyApp());
+  runApp(const FirebaseInitializer(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
